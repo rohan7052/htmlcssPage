7f24b420df474ff47ee44c4187d9054cfcac3e12
rohan singh <rohan.singh184@gmail.com>
2021-07-10 16:10:44 +0530

    xcydyg

---
+++ NewFolder1/redhot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace htmlcssPage.NewFolder1
+{
+    public class redhot
+    {
+        public int id { get; set; }
+    }
+}
